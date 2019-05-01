45ed6466ecbf7ba5efae6a4c9159700c24cb4603
Christopher Muñoz <cmunozcortes@me.com>
2019-04-30 22:31:04 -0400

    Create project3.py

---
+++ project3.py
@@ -0,0 +1,25 @@
+"""
+Question 1: Compute the sparsity of the movie rating dataset, where sparsity is defined by:
+sparsity = total num of available ratings / total num of possible ratings
+"""
+
+"""
+Question 2: Question 2: Plot a histogram showing the frequency of the rating values
+"""
+
+"""
+Question 3: Plot the distribution of the number of ratings received among movies
+"""
+
+"""
+Question 4: Plot the distribution of ratings among users
+"""
+
+"""
+Question 5: Explain the salient features of the distribution found in question 3 and their 
+implications for the recommendation process
+"""
+
+"""
+Question 6: Compute the variance of the rating values received by each movie
+""""
